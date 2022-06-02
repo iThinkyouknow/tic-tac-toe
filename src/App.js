f3595272c683b291fbb9130ce964cdbf3e9b5451
iThinkyouknow <woontlt.dev@gmail.com>
2022-06-03 01:46:15 +0800

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,96 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+const chars = ['', 'X', 'O'];
+
+const genDefaultGrid = () => {
+  return Array.from({ length: 3 }, () => Array.from({ length: 3 }, () => 0));
+};
+
+export const checkVictory = (grid, y, x, currentPlayerNumber) => {
+  const row = grid[y];
+
+  const col = grid.map(row => row[x]);
+
+  if ([row, col].some(arr => arr.every(num => num === currentPlayerNumber))) return true;
+
+  const diagonalWin = [[0, 1, 2], [2, 1, 0]].some(arr => {
+    return arr.every((y1, x1) => {
+      return grid[y1][x1] === currentPlayerNumber;
+    });
+  });
+
+  if (diagonalWin) return true;
+
+  return false;
+};
+
 function App() {
+  const [currentPlayer, setCurrentPlayer] = useState(true);
+  let [grid, setGrid] = useState(genDefaultGrid());
+  const [victory, setVictory] = useState(false);
+
+  const currentPlayerNumber = currentPlayer
+    ? 1
+    : 2;
+
+  const onClickBtn = (y, x) => () => {
+    grid[y][x] = currentPlayerNumber;
+    const isVic = checkVictory(grid, y, x, currentPlayerNumber);
+
+    if (isVic) {
+      setVictory(isVic);
+    } else {
+      setCurrentPlayer(!currentPlayer);
+    }
+    setGrid([...grid]);
+  };
+
+  const reset = () => {
+    setGrid(genDefaultGrid());
+    setCurrentPlayer(true);
+    setVictory(false);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <div class='flex items-center'>
+        <div className='flex-col'>
+          {
+            grid.map((arr, y) => {
+              return (
+                <div className='flex'>
+                  {
+                    arr.map((int, x) => {
+                      return (
+                        <button
+                          key={`${y}${x}`}
+                          onClick={onClickBtn(y, x)}
+                          disabled={int || victory}
+                          className='tic-tac-toc-btn'>
+                          {chars[int]}
+                        </button>
+                      )
+                    })
+                  }
+                </div>
+              )
+            })
+          }
+        </div>
+        <p class='player-label margin-left-16px font-size-50px flex flex-col'>
+          <span>Player {currentPlayerNumber}</span>
+          {
+            victory && <span>Is Victorious!</span>
+          }
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+
+        <button
+          className='reset-btn'
+          onClick={reset}
         >
-          Learn React
-        </a>
-      </header>
+          Reset
+        </button>
+      </div>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,97 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import { checkVictory } from './App';
+
+test('check victory conditions', () => {
+  [
+    {
+      grid: [
+        [1, 1, 1],
+        [0, 0, 0],
+        [0, 0, 0]
+      ],
+      y: 0,
+      x: 2,
+      currPlayer: 1,
+      result: true,
+    },
+    {
+      grid: [
+        [1, 0, 1],
+        [0, 1, 0],
+        [0, 0, 1]
+      ],
+      y: 1,
+      x: 1,
+      currPlayer: 1,
+      result: true,
+    },
+    {
+      grid: [
+        [1, 0, 1],
+        [0, 1, 0],
+        [0, 0, 1]
+      ],
+      y: 2,
+      x: 2,
+      currPlayer: 1,
+      result: true,
+    },
+    {
+      grid: [
+        [1, 0, 1],
+        [0, 1, 0],
+        [0, 0, 0]
+      ],
+      y: 2,
+      x: 2,
+      currPlayer: 1,
+      result: false,
+    },
+    {
+      grid: [
+        [1, 0, 1],
+        [1, 1, 1],
+        [0, 0, 0]
+      ],
+      y: 1,
+      x: 0,
+      currPlayer: 1,
+      result: true,
+    },
+    {
+      grid: [
+        [1, 2, 1],
+        [1, 2, 1],
+        [0, 2, 0]
+      ],
+      y: 2,
+      x: 1,
+      currPlayer: 2,
+      result: true,
+    },
+    {
+      grid: [
+        [1, 0, 2],
+        [1, 0, 2],
+        [0, 1, 2]
+      ],
+      y: 0,
+      x: 2,
+      currPlayer: 2,
+      result: true,
+    },
+    {
+      grid: [
+        [1, 0, 2],
+        [1, 0, 2],
+        [0, 1, 2]
+      ],
+      y: 0,
+      x: 2,
+      currPlayer: 1,
+      result: false,
+    }
+  ].forEach(({ grid, y, x, currPlayer, result }) => {
+    expect(checkVictory(grid, y, x, currPlayer)).toBe(result);
+  })
 });
